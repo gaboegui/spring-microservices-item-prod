5deb5d6138c364650fcdaff0c581a3fac578d8e0
Gabriel Eguiguren <gabo.egui@gmail.com>
2021-01-30 18:32:41 -0500

    Añadir Config Server y Spring Boot Actuator

--- ude-micro-service-item-producto-2/src/main/java/ec/pymeapps/micro/app/item/controller/ItemController.java
+++ ude-micro-service-item-producto-2/src/main/java/ec/pymeapps/micro/app/item/controller/ItemController.java
@@ -1,10 +1,17 @@
 package ec.pymeapps.micro.app.item.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,17 +22,60 @@ import ec.pymeapps.micro.app.item.models.Item;
 import ec.pymeapps.micro.app.item.models.Producto;
 import ec.pymeapps.micro.app.item.models.service.ItemService;
 
+
+
+/**
+ * @RefreshScope forza a que se consulten de nuevo los datos del
+ * servidor de configuracion, en conjuto con el Spring Boot Actuator
+ * que se configura en bootstrap.properties: 
+ *     	management.endpoints.web.exposure.include=*
+ * 
+ * se ejecuta con una llamada POST al URL:
+ * 		localhost:8005/actuator/refresh
+ * 
+ * 
+ * @author Gabriel E.
+ *
+ */
+@RefreshScope
 @RestController
 public class ItemController {
 	
+	//@Qualifier("serviceRestTemplate")
 	@Autowired
-	//@Qualifier("serviceRestTemplate")   
 	@Qualifier("servicio-Productos-Feign")
 	private ItemService service;
 	
+	
+	// lee la propiedad obtenida por el servidor de configuracion
+	// definido en bootstrap.properties
+	@Value("${cloud-configuracion.texto}")
+	private String mensaje;
+	
+	
+	@Autowired
+	Environment env;
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		
+		Map<String,String> json = new HashMap<String,String>();
+		json.put("texto", mensaje);
+		json.put("puerto", puerto);
+		
+		// pregunto si estoy en ambiente desarrollo
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor-mantenimiento", env.getProperty("cloud-configuracion.autor.mantenimiento"));
+			json.put("autor-email", env.getProperty("cloud-configuracion.autor.email"));
+			
+		}
+				
+		return new ResponseEntity<Map<String,String>>(json, HttpStatus.OK);
+		
+	}
+	
 	@GetMapping("/listar")
 	public List<Item> listar(){
-		
 		return service.findAll();
 	}
 	
@@ -54,8 +104,6 @@ public class ItemController {
 		Item item = new Item(prod, cantidad);
 		
 		return item;
-		
-		
 	}
 
 }
