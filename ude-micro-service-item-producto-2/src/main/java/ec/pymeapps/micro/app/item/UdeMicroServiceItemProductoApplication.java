2fda5f6db32f07ad885840cd6f19719025dca489
Gabriel Eguiguren <gabo.egui@gmail.com>
2021-01-29 23:18:10 -0500

    Funcionando Eureka Config

--- ude-micro-service-item-producto-2/src/main/java/ec/pymeapps/micro/app/item/UdeMicroServiceItemProductoApplication.java
+++ ude-micro-service-item-producto-2/src/main/java/ec/pymeapps/micro/app/item/UdeMicroServiceItemProductoApplication.java
@@ -2,18 +2,29 @@ package ec.pymeapps.micro.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * utilizamos Client ya que solo hay un cleinte si consumieramos mas 
  * servicios se utilizaria @RibbonClients 
- * 
+ *
+ *  Al aumentar la dependencia en POM.xml de
+ *  spring-cloud-starter-netflix-eureka-client
+ *  se registra automaticamente el servicio, pero no esta de mas poner 
+ *  la anotacion  @EnableEurekaClient de forma explicita
+ *  
+ *  Asi mismo al añadir eureka-client, ya no es necesaria
+ *  la anotacion @RibbonClient(name = "pymeapps.servicio.producto")
+ *  
+ *  
  * 
  * @author Editor
  *
  */
-@RibbonClient(name = "pymeapps.servicio.producto")
+
+//@RibbonClient(name = "pymeapps.servicio.producto")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class UdeMicroServiceItemProductoApplication {
